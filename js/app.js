31d6d9dac9a45f2f22125a71be6f291a887b38a7
user name <user email>
2022-07-20 02:46:05 +0300

    task13

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ let allFood = [];
 const main = document.getElementsByTagName('main');
 const table = document.createElement('table');
 table.className = 'table';
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+
 // create a constructor
 function Food(foodName, foodType, price) {
      
@@ -89,5 +93,77 @@ Food.prototype.render=function(){
     table.appendChild(row)
     main[0].appendChild(table)
      
-}
- 
+
+  }
+// the name of the drink is the label
+//the price of the drink is the data
+
+const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Rice', 'Broccoli', 'Bread', 'meet', 'soap'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2, 3, 1, 6, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const myChart2 = new Chart(ctx2, {
+  type: 'doughnut',
+  data: {
+      labels: ['Rice', 'Broccoli', 'Bread', 'meet', 'soap'],
+      datasets: [{
+          label: '# of Votes',
+          data: [2, 3, 1, 6, 2],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+  }
+});
